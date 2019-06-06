5fddb6cf43e545d5bdaf01c4f5360f2fae62fa0c
Nannerss <nannerssbusiness@gmail.com>
2019-06-05 20:19:07 -0700

    Improved the listing system

--- src/main/java/com/nannerss/craftcontrol/commands/CraftControlCommand.java
+++ src/main/java/com/nannerss/craftcontrol/commands/CraftControlCommand.java
@@ -3,13 +3,20 @@ package com.nannerss.craftcontrol.commands;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.commons.lang.WordUtils;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
+import com.nannerss.bananalib.messages.Component;
 import com.nannerss.bananalib.messages.Messages;
+import com.nannerss.bananalib.utils.Utils;
 import com.nannerss.craftcontrol.CraftControl;
 import com.nannerss.craftcontrol.Settings;
+
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.TextComponent;
 
 public class CraftControlCommand extends Command {
     
@@ -24,20 +31,27 @@ public class CraftControlCommand extends Command {
     public boolean execute(CommandSender sender, String commandLabel, String[] args) {
         final List<Material> BANNED_MATERIALS = Settings.BANNED_MATERIALS;
         
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("&cYou must be a player to use this command!");
+            return false;
+        }
+        
+        Player p = (Player) sender;
+        
         if (args.length < 1) {
-            Messages.sendMessage(sender, "&bRunning &3CraftControl &bv" + CraftControl.getInstance().getDescription().getVersion() + "!");
+            Messages.sendMessage(p, "&bRunning &3CraftControl &bv" + CraftControl.getInstance().getDescription().getVersion() + "!");
             return false;
         }
         
         String parameter = args[0];
         
         if ("help".equalsIgnoreCase(parameter)) {
-            if (!sender.hasPermission("craftcontrol.help")) {
-                Messages.sendMessage(sender, "&cInsufficient permissions!");
+            if (!p.hasPermission("craftcontrol.help")) {
+                Messages.sendMessage(p, "&cInsufficient permissions!");
                 return false;
             }
             
-            Messages.sendMessage(sender,
+            Messages.sendMessage(p,
                     "",
                     "     &3&lShowing help for CraftControl...",
                     "",
@@ -48,39 +62,45 @@ public class CraftControlCommand extends Command {
                     "&b/craftcontrol &3reload &8- &7Reloads the config.",
                     "");
         } else if ("list".equalsIgnoreCase(parameter)) {
-            if (!sender.hasPermission("craftcontrol.list")) {
-                Messages.sendMessage(sender, "&cInsufficient permissions!");
+            if (!p.hasPermission("craftcontrol.list")) {
+                Messages.sendMessage(p, "&cInsufficient permissions!");
                 return false;
             }
             
-            Messages.sendMessage(sender,
+            Messages.sendMessage(p,
+                    "",
                     "",
                     "     &3&lShowing blacklisted items...",
                     "");
             
             for (Material material : BANNED_MATERIALS) {
-                Messages.sendMessage(sender, "&8- &b" + material.toString());
+                BaseComponent[] message = Component.builder(" &8- ").append("&b" + WordUtils.capitalize(material.toString().toLowerCase().replace("_", " "))).onHover("&b&lClick &r&7to remove!").onClickRunCmd("/craftcontrol remove " + material.toString()).create();
+                p.spigot().sendMessage(message);
+            }
+    
+            if (BANNED_MATERIALS.isEmpty()) {
+                Messages.sendMessage(p, " &c&oNo materials have been added to the list...");
             }
             
-            Messages.sendMessage(sender, "");
+            Messages.sendMessage(p, "", "");
         } else if ("reload".equalsIgnoreCase(parameter)) {
-            if (!sender.hasPermission("craftcontrol.reload")) {
-                Messages.sendMessage(sender, "&cInsufficient permissions!");
+            if (!p.hasPermission("craftcontrol.reload")) {
+                Messages.sendMessage(p, "&cInsufficient permissions!");
                 return false;
             }
             
             CraftControl.getSettings().loadConfig();
             Settings.load();
             
-            Messages.sendMessage(sender, "&bSuccessfully reloaded the config!");
+            Messages.sendMessage(p, "&bSuccessfully reloaded the config!");
         } else if ("add".equalsIgnoreCase(parameter)) {
-            if (!sender.hasPermission("craftcontrol.add")) {
-                Messages.sendMessage(sender, "&cInsufficient permissions!");
+            if (!p.hasPermission("craftcontrol.add")) {
+                Messages.sendMessage(p, "&cInsufficient permissions!");
                 return false;
             }
             
             if (args.length < 2) {
-                Messages.sendMessage(sender, "&cUsage: /craftcontrol add <material>");
+                Messages.sendMessage(p, "&cUsage: /craftcontrol add <material>");
                 return false;
             }
             
@@ -90,7 +110,7 @@ public class CraftControlCommand extends Command {
             try {
                 material = Material.valueOf(materialName);
             } catch (IllegalArgumentException e) {
-                Messages.sendMessage(sender, "&c" + materialName + " is not a valid material!");
+                Messages.sendMessage(p, "&c" + materialName + " is not a valid material!");
                 return false;
             }
     
@@ -98,22 +118,22 @@ public class CraftControlCommand extends Command {
             Settings.load();
             
             if (Settings.BANNED_MATERIALS.contains(material)) {
-                Messages.sendMessage(sender, "&c" + materialName + " is already blacklisted!");
+                Messages.sendMessage(p, "&c" + materialName + " is already blacklisted!");
                 return false;
             }
             
             Settings.BANNED_MATERIALS.add(material);
             Settings.save();
             
-            Messages.sendMessage(sender, "&bAdded " + materialName + " to the list of blacklisted recipes!");
+            Messages.sendMessage(p, "&bAdded " + materialName + " to the list of blacklisted recipes!");
         } else if ("remove".equalsIgnoreCase(parameter)) {
-            if (!sender.hasPermission("craftcontrol.remove")) {
-                Messages.sendMessage(sender, "&cInsufficient permissions!");
+            if (!p.hasPermission("craftcontrol.remove")) {
+                Messages.sendMessage(p, "&cInsufficient permissions!");
                 return false;
             }
     
             if (args.length < 2) {
-                Messages.sendMessage(sender, "&cUsage: /craftcontrol remove <material>");
+                Messages.sendMessage(p, "&cUsage: /craftcontrol remove <material>");
                 return false;
             }
     
@@ -123,7 +143,7 @@ public class CraftControlCommand extends Command {
             try {
                 material = Material.valueOf(materialName);
             } catch (IllegalArgumentException e) {
-                Messages.sendMessage(sender, "&c" + materialName + " is not a valid material!");
+                Messages.sendMessage(p, "&c" + materialName + " is not a valid material!");
                 return false;
             }
     
@@ -131,14 +151,14 @@ public class CraftControlCommand extends Command {
             Settings.load();
     
             if (!Settings.BANNED_MATERIALS.contains(material)) {
-                Messages.sendMessage(sender, "&c" + materialName + " is not a blacklisted recipe!");
+                Messages.sendMessage(p, "&c" + materialName + " is not a blacklisted recipe!");
                 return false;
             }
     
             Settings.BANNED_MATERIALS.remove(material);
             Settings.save();
     
-            Messages.sendMessage(sender, "&bRemoved " + materialName + " from the list of blacklisted recipes!");
+            Messages.sendMessage(p, "&bRemoved " + materialName + " from the list of blacklisted recipes!");
         }
         
         return false;
